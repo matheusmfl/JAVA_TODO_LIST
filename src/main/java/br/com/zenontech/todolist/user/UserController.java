e9ee2186ea9adc536608593de4352ebfc9166292
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-11 13:13:15 -0300

    Create interface  Contract IUserRepository, create method Post applying the contract

--- src/main/java/br/com/zenontech/todolist/user/UserController.java
+++ src/main/java/br/com/zenontech/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.zenontech.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void Create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUserName());
+  public UserModel Create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+
+    return userCreated;
   }
 }
