148a12f56e2ffcd5919cef80de0ca913b0888a9b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-10 23:16:56 -0300

    create First Model and UserController

---
+++ src/main/java/br/com/zenontech/todolist/user/UserController.java
@@ -0,0 +1,16 @@
+package br.com.zenontech.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @PostMapping("/")
+  public void Create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.Name);
+  }
+}
---
+++ src/main/java/br/com/zenontech/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.zenontech.todolist.user;
+
+public class UserModel {
+  public String userName;
+  public String Name;
+  public String Password;
+}
