a3e91350d0a3d50bc657569ac9baa1132ef8eb6a
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-11 12:14:03 -0300

    add Dependency H2 localstorage, springJPA and setup enviroments

--- src/main/java/br/com/zenontech/todolist/user/UserModel.java
+++ src/main/java/br/com/zenontech/todolist/user/UserModel.java
@@ -1,31 +1,11 @@
 package br.com.zenontech.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   public String userName;
   public String name;
   public String password;
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-
-  public String getUserName() {
-    return userName;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 }
