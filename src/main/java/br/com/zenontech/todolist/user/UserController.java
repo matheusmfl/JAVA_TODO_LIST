28f9dbd44ce531a51fb3a3df3ff4c2693ebebbea
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-11 23:09:19 -0300

    create Hash Password using Bcrypt

--- src/main/java/br/com/zenontech/todolist/user/UserController.java
+++ src/main/java/br/com/zenontech/todolist/user/UserController.java
@@ -1,10 +1,13 @@
 package br.com.zenontech.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -14,14 +17,18 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel Create(@RequestBody UserModel userModel) {
+  public ResponseEntity Create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
-      System.out.println("Usuário já cadastrado");
+      return ResponseEntity.status(400).body("User already exists");
     }
+
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashed);
     var userCreated = this.userRepository.save(userModel);
 
-    return userCreated;
+    return ResponseEntity.status(201).body(userCreated);
   }
 }
