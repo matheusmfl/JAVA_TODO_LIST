42d6bf95421d39f72a662fa24700750257b91280
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-16 16:24:16 -0300

    create business rule datetime must be less the current date on the task Controller

--- src/main/java/br/com/zenontech/todolist/tasks/TaskController.java
+++ src/main/java/br/com/zenontech/todolist/tasks/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.zenontech.todolist.tasks;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,16 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartedAt())) {
+      return ResponseEntity.status(400).body("The start date must be less than the current date ");
+    }
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(200).body(task);
   }
 }
