8f034dd19ed102a91d627df73f14142506e49ddc
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-16 18:38:56 -0300

    create Util update data, persist case null

--- src/main/java/br/com/zenontech/todolist/tasks/TaskController.java
+++ src/main/java/br/com/zenontech/todolist/tasks/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zenontech.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -51,9 +52,13 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+    var idUser = request.getAttribute("idUser");
+
+    var task = this.taskRepository.findById(id).orElse(null);
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
