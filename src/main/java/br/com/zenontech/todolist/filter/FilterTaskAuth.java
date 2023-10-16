3c9ab474b5779719e3ff686a808757fea6628024
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-15 21:32:18 -0300

    Validate Pass and user on the userModel

--- src/main/java/br/com/zenontech/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/zenontech/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.zenontech.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.zenontech.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
@@ -14,6 +17,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -28,7 +34,22 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     String[] credentials = auth_string.split(":");
     String username = credentials[0];
     String password = credentials[1];
-    filterChain.doFilter(request, response);
+
+    var user = this.userRepository.findByUsername(username);
+
+    if (user == null) {
+      response.sendError(401, "User does not exist");
+    } else {
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+      if (passwordVerify.verified) {
+        filterChain.doFilter(request, response);
+      } else {
+        response.sendError(401, "Unathourized password");
+      }
+
+    }
+
   }
 
 }
